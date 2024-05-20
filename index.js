754c93197914730e85aceee7f52ef02c2fe03d64
GooZzi <140390407+GooZzi@users.noreply.github.com>
2024-05-20 18:19:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
 const dotenv = require('dotenv');
-
-dotenv.config();
 const { next } = require('grammy');
 const { Bot, GrammyError } = require('grammy');
+
+dotenv.config();
+
 const bot = new Bot(process.env.BOT_API_KEY);
+const queue = [];
+let lastSent = Date.now();
+let messagesSent = 0;
 
 const currentHour = new Date().getUTCHours() + 3; // Получаем текущий час в формате UTC и добавляем 3 часа для перевода в МСК
-
 
 function escapeMarkdownV2(text) {
     const escapeChars = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!'];
     return text.split('').map(c => escapeChars.includes(c) ? `\\${c}` : c).join('');
 }
 
+function sendMessage(groupId, message) {
+  queue.push({groupId, message});
+}
+
+function processQueue() {
+  if (queue.length > 0 && (Date.now() - lastSent > 1000 || messagesSent < 30)) {
+    const {groupId, message} = queue.shift();
+    bot.api.sendMessage(groupId, escapeMarkdownV2(message), { parse_mode: 'MarkdownV2' })
+      .then(() => {
+        lastSent = Date.now();
+        messagesSent++;
+        if (messagesSent >= 30) {
+          messagesSent = 0;
+          lastSent = Date.now();
+        }
+      })
+      .catch((error) => {
+        console.error('Ошибка при отправке сообщения:', error);
+      });
+  }
+  setTimeout(processQueue, 100);
+}
+
+processQueue();
+
 bot.on('message', async (ctx) => {
-    const authorizedUsers = []; // Замените на реальные ID пользователей
-    const groupChatsTime = ['-1001980493060', '-4060260673']; // Замените на реальные ID групп
+    const authorizedUsers = ['785492955']; // Замените на реальные ID пользователей
+    const groupChatsTime = ['-1001980493060', '-1001728011656','-1001757183507','-669874108','-1001857175790','-1001845076622']; // Замените на реальные ID групп
     const chatId = ctx.chat?.id.toString();
     // Проверяем, является ли чат одной из указанных групп и находится ли текущее время в указанном диапазоне
       if (groupChatsTime.includes(chatId) && (currentHour < 10 || currentHour > 22)) {
@@ -48,16 +76,11 @@ bot.command('push', async (ctx) => {
 
         const formattedMessage = `**${messageText}**`;
 
-       try {
-    for (const chatId of groupChats) {
-        await bot.api.sendMessage(chatId, escapeMarkdownV2(formattedMessage), { parse_mode: 'MarkdownV2' });
-    }
-
-            await ctx.reply('Сообщение было успешно отправлено в группы.');
-        } catch (error) {
-            console.error('Ошибка при отправке сообщения:', error);
-            await ctx.reply('Произошла ошибка при отправке вашего сообщения.');
+        for (const chatId of groupChats) {
+            sendMessage(chatId, formattedMessage);
         }
+
+        await ctx.reply('Сообщение было успешно отправлено в группы.');
     } else {
         await ctx.reply('У вас нет доступа для использования этой команды.');
     }
