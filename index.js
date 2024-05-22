a5970562ef2f5798c8d794c81210daaa14aaa212
GooZzi <140390407+GooZzi@users.noreply.github.com>
2024-05-22 18:15:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,13 +11,36 @@ let messagesSent = 0;
 
 const currentHour = new Date().getUTCHours() + 3; // Получаем текущий час в формате UTC и добавляем 3 часа для перевода в МСК
 
+bot.on('message', async (ctx) => {
+  const authorizedUsers = []; // Замените на реальные ID пользователей
+  const groupChatsTime = ['-1001980493060']; // Замените на реальные ID групп
+  const chatId = ctx.chat?.id.toString();
+  // Проверяем, является ли чат одной из указанных групп и находится ли текущее время в указанном диапазоне
+    if (groupChatsTime.includes(chatId) && (currentHour < 10 || currentHour > 23)) {
+      // Если условия выполняются, проверяем, является ли пользователь авторизованным
+      const isAuthorized = authorizedUsers.includes(ctx.from.id.toString());
+      if (!isAuthorized) {
+        // Если пользователь не авторизован, отправляем сообщение о начале работы группы
+        if (ctx.message?.new_chat_members || ctx.message?.left_chat_member) {
+            // Если условия выполняются, игнорируем сообщение
+            await next();
+        }
+        await ctx.reply('К сожалению, режим работы чата с 10:00. Обратитесь за помощью в поддержку на платформе, в соответствующем разделе.');
+      }
+  } else {
+      // Если условия не выполняются, продолжаем обработку сообщений
+      await next();
+  }
+});
+
+
 function escapeMarkdownV2(text) {
-    const escapeChars = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!'];
-    return text.split('').map(c => escapeChars.includes(c) ? `\\${c}` : c).join('');
+  const escapeChars = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!'];
+  return text.split('').map(c => escapeChars.includes(c) ? `\\${c}` : c).join('');
 }
 
 function sendMessage(groupId, message) {
-  queue.push({groupId, message});
+queue.push({groupId, message});
 }
 
 function processQueue() {
@@ -40,59 +63,5 @@ function processQueue() {
 }
 
 processQueue();
-
-
-bot.command('push', async (ctx) => {
-  const authorizedUsers = ['785492955']; // Замените на реальные ID пользователей
-  const groupChats = ['-1001980493060','-1001728011656','-1001757183507','-669874108','-1001857175790','-1001845076622']; // Замените на реальные ID групп
-  if (authorizedUsers.includes(ctx.from.id.toString())) {
-      const messageText = ctx.message.text.slice(6);
-
-      if (messageText.trim() === '') {
-          await ctx.reply('Пожалуйста, добавьте текст сообщения после команды /push.');
-          return;
-      }
-
-      const formattedMessage = `${messageText}`;
-
-      for (const chatId of groupChats) {
-          sendMessage(chatId, formattedMessage);
-      }
-
-      await ctx.reply('Сообщение было успешно отправлено в группы.');
-  } else {
-      await ctx.reply('У вас нет доступа для использования этой команды.');
-  }
-});
-
-bot.on('message', async (ctx) => {
-    const authorizedUsers = ['785492955', '1957119524']; // Замените на реальные ID пользователей
-    const groupChatsTime = ['-1001980493060']; // Замените на реальные ID групп
-    const chatId = ctx.chat?.id.toString();
-    // Проверяем, является ли чат одной из указанных групп и находится ли текущее время в указанном диапазоне
-      if (groupChatsTime.includes(chatId) && (currentHour < 10 || currentHour > 23)) {
-        // Если условия выполняются, проверяем, является ли пользователь авторизованным
-        const isAuthorized = authorizedUsers.includes(ctx.from.id.toString());
-        if (!isAuthorized) {
-          // Если пользователь не авторизован, отправляем сообщение о начале работы группы
-          if (ctx.message?.new_chat_members || ctx.message?.left_chat_member) {
-              // Если условия выполняются, игнорируем сообщение
-              await next();
-          }
-          await ctx.reply('К сожалению, режим работы чата с 10:00. Обратитесь за помощью в поддержку на платформе, в соответствующем разделе.');
-        }
-    } else {
-        // Если условия не выполняются, продолжаем обработку сообщений
-        await next();
-    }
-  });
-
-
-  
-bot.catch('err', async(ctx) => {
-    console.error('Ошибка бота:', err);
-    await ctx.reply('Ошибка');
-    
-});
 
 bot.start();
